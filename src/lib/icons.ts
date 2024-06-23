b0d45845c0f838eea44eb96a6c69e819bd52c3d2
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2024-06-23 11:03:17 +0200

    icons and menu

---
+++ src/lib/icons.ts
@@ -0,0 +1,45 @@
+// icons.ts
+import { LucideIcon, Apple, Banana, Bean, Beef, Carrot, Cherry, Citrus, Drumstick, Egg, EggFried, Fish, Grape, Ham, Milk, Nut, Pizza, Salad, Sandwich, Snail, Vegan, Wheat } from 'lucide-react';
+
+export interface CategoryOption {
+    value: string;
+    label: string;
+    icon: LucideIcon;
+    color: string;
+}
+
+export const categoryOptions: CategoryOption[] = [
+    { value: 'breakfast', label: 'Breakfast', icon: Apple, color: 'bg-yellow-100 text-yellow-700'},
+    { value: 'lunch', label: 'Lunch', icon: Banana, color: 'bg-green-100 text-green-700'},
+    { value: 'dinner', label: 'Dinner', icon: Bean, color: 'bg-red-100 text-red-700'},
+    { value: 'supper', label: 'Supper', icon: Beef, color: 'bg-blue-100 text-blue-700'},
+];
+
+export interface IconOption {
+  value: string;
+  icon: LucideIcon;
+}
+
+export const iconOptions: IconOption[] = [
+    { value: 'apple', icon: Apple },
+    { value: 'banana', icon: Banana },
+    { value: 'bean', icon: Bean },
+    { value: 'beef', icon: Beef },
+    { value: 'carrot', icon: Carrot },
+    { value: 'cherry', icon: Cherry },
+    { value: 'citrus', icon: Citrus },
+    { value: 'drumstick', icon: Drumstick },
+    { value: 'egg', icon: Egg },
+    { value: 'eggFried', icon: EggFried },
+    { value: 'fish', icon: Fish },
+    { value: 'grape', icon: Grape },
+    { value: 'ham', icon: Ham },
+    { value: 'milk', icon: Milk },
+    { value: 'nut', icon: Nut },
+    { value: 'pizza', icon: Pizza },
+    { value: 'salad', icon: Salad },
+    { value: 'sandwich', icon: Sandwich },
+    { value: 'snail', icon: Snail },
+    { value: 'vegan', icon: Vegan },
+    { value: 'wheat', icon: Wheat },
+  ];
---
+++ src/lib/menu-list.ts
@@ -0,0 +1,106 @@
+import {
+  Tag,
+  Dumbbell,
+  ShoppingCart,
+  CalendarCheck2,
+  Settings,
+  Bookmark,
+  SquarePen,
+  LayoutGrid,
+  ScrollText
+} from "lucide-react";
+
+type Submenu = {
+  href: string;
+  label: string;
+  active: boolean;
+  icon: any;
+};
+
+type Menu = {
+  href: string;
+  label: string;
+  active: boolean;
+  icon: any;
+  submenus: Submenu[];
+};
+
+type Group = {
+  groupLabel: string;
+  menus: Menu[];
+};
+
+export function getMenuList(pathname: string): Group[] {
+  return [
+    {
+      groupLabel: "",
+      menus: [
+        {
+          href: "/dashboard",
+          label: "Dashboard",
+          active: pathname === "/dashboard",
+          icon: LayoutGrid,
+          submenus: []
+        }
+      ]
+    },
+    {
+      groupLabel: "Content",
+      menus: [
+        {
+          href: "/dashboard/products",
+          label: "Products",
+          active: pathname === "/dashboard/products",
+          icon: ScrollText,
+          submenus: []
+        },
+        {
+          href: "/dashboard/diet",
+          label: "Diet Planner",
+          active: pathname === "/dashboard/diet",
+          icon: Bookmark,
+          submenus: []
+        },
+        {
+          href: "/dashboard",
+          label: "Workout",
+          active: pathname === "/dashboard/workout",
+          icon: Dumbbell,
+          submenus: []
+        },
+        {
+          href: "/schedule",
+          label: "Schedule",
+          active: pathname === "/schedule",
+          icon: CalendarCheck2,
+          submenus: [
+            {
+              href: "/dashboard/schedule/food",
+              label: "Food",
+              active: pathname === "/dashboard/schedule/food",
+              icon: SquarePen,
+            },
+            {
+              href: "/dashboard",
+              label: "Workout",
+              active: pathname === "/schedule/workout",
+              icon: SquarePen,
+            }
+          ]
+        }
+      ]
+    },
+    {
+      groupLabel: "Settings",
+      menus: [
+        {
+          href: "/dashboard/account",
+          label: "Account",
+          active: pathname === "/dashboard/account",
+          icon: Settings,
+          submenus: []
+        }
+      ]
+    }
+  ];
+}
