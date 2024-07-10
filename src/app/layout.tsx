628456de2a0a8e275d7817188bf676eff211aa34
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2024-07-10 18:22:49 +0200

    app font update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,20 @@
-import {
-  ClerkProvider,
-
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import "../styles/globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-
   return (
     <ClerkProvider
       appearance={{
@@ -19,7 +22,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
       }}
     >
       <html lang="en" suppressHydrationWarning>
-        <body className="dark">
+        <body
+          className={cn(
+            "min-h-screen dark bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
           <main>
             <ThemeProvider
               attribute="class"
