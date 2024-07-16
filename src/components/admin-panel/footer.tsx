0acdb634144a514f851445adca744a9dfb199b55
DahlGitHub <78036458+DahlGitHub@users.noreply.github.com>
2024-07-16 09:10:08 +0200

    Update footer.tsx

--- src/components/admin-panel/footer.tsx
+++ src/components/admin-panel/footer.tsx
@@ -5,25 +5,7 @@ export function Footer() {
     <div className="z-20 w-full bg-zinc-950 shadow backdrop-blur border-t border-zinc-900 ">
       <div className="mx-4 md:mx-8 flex h-14 items-center">
         <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
-          Built with love{" "}
-          <Link
-            href="https://ui.shadcn.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            shadcn/ui
-          </Link>
-          . The source code is available on{" "}
-          <Link
-            href="https://github.com/salimi-my/shadcn-ui-sidebar"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-medium underline underline-offset-4"
-          >
-            GitHub
-          </Link>
-          .
+          Built with love, Adrian @ 2024 - Currently not mobile friendly.
         </p>
       </div>
     </div>
